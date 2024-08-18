ffbc382257093e38e1a3a59446fcf0bc31be203c
arularun-developer <arularunoffical1110@gmail.com>
2024-08-18 13:23:22 +0530

    cors package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // app.js
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import cors
 const connectDB = require('./db');
 const userRoutes = require('./Routes/userroute');
 const expenseRoutes = require('./Routes/expenseroute');
@@ -10,6 +11,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors()); // Use cors middleware
 app.use(express.json());
 
 // Add a route for the root URL to send the welcome message
@@ -23,4 +25,3 @@ app.use('/expenses', expenseRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
